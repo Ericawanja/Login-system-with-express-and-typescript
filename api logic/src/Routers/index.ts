7b9a4b9acc2c2634a0d48c2f7cc657672a0d7171
Ericawanja <wanjaerica@gmail.com>
2023-02-24 10:03:13 +0300

    fixed bug on the ejs templates

--- api logic/src/Routers/index.ts
+++ api logic/src/Routers/index.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { login, register, forgotPassword, resetPassword } from "../Controllers/index";
 import { validator } from "../middlewares/validator";
-import { loginSchema, registerSchema } from "../schemas";
-import { forgotPasswordSchema } from '../schemas/index';
+import { loginSchema, registerSchema, forgotPasswordSchema } from "../schemas";
+
 
 const authRouter = Router();
 
 authRouter.post("/register", validator(registerSchema), register);
 authRouter.post("/login", validator(loginSchema), login);
 authRouter.post("/forgotpassword",validator(forgotPasswordSchema), forgotPassword);
-authRouter.post("/resetPassword", validator(loginSchema), resetPassword)
+authRouter.post("/reset", validator(loginSchema), resetPassword)
 
 export default authRouter;
